ac06a81e631dbe14a832c38a5b90f7052d512f24
Dmitry Samodov <samodov.dmitry@gmail.com>
2020-06-15 13:54:16 +0300

    LM-208: added asset/asset pair validation for http

--- src/HftApi/WebApi/AssetPairsController.cs
+++ src/HftApi/WebApi/AssetPairsController.cs
@@ -2,7 +2,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using HftApi.WebApi.Models;
 using Lykke.HftApi.Domain.Entities;
+using Lykke.HftApi.Domain.Exceptions;
 using Lykke.HftApi.Domain.Services;
+using Lykke.HftApi.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,10 +15,15 @@ namespace HftApi.WebApi
     public class AssetPairsController : ControllerBase
     {
         private readonly IAssetsService _assetsService;
+        private readonly ValidationService _validationService;
 
-        public AssetPairsController(IAssetsService assetsService)
+        public AssetPairsController(
+            IAssetsService assetsService,
+            ValidationService validationService
+            )
         {
             _assetsService = assetsService;
+            _validationService = validationService;
         }
 
         [HttpGet]
@@ -31,6 +38,11 @@ namespace HftApi.WebApi
         [ProducesResponseType(typeof(ResponseModel<AssetPair>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAssetPairAsync(string assetPairId)
         {
+            var result = await _validationService.ValidateAssetPairAsync(assetPairId);
+
+            if (result != null)
+                throw HftApiException.Create(result.Code, result.Message).AddField(result.FieldName);
+
             var assetPair = await _assetsService.GetAssetPairByIdAsync(assetPairId);
             return Ok(ResponseModel<AssetPair>.Ok(assetPair));
         }
--- src/HftApi/WebApi/AssetsController.cs
+++ src/HftApi/WebApi/AssetsController.cs
@@ -1,9 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using HftApi.WebApi.Models;
 using Lykke.HftApi.Domain.Entities;
+using Lykke.HftApi.Domain.Exceptions;
 using Lykke.HftApi.Domain.Services;
+using Lykke.HftApi.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,10 +15,15 @@ namespace HftApi.WebApi
     public class AssetsController : ControllerBase
     {
         private readonly IAssetsService _assetsService;
+        private readonly ValidationService _validationService;
 
-        public AssetsController(IAssetsService assetsService)
+        public AssetsController(
+            IAssetsService assetsService,
+            ValidationService validationService
+            )
         {
             _assetsService = assetsService;
+            _validationService = validationService;
         }
 
         [HttpGet]
@@ -32,6 +38,11 @@ namespace HftApi.WebApi
         [ProducesResponseType(typeof(ResponseModel<Asset>), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAssetAsync(string assetId)
         {
+            var result = await _validationService.ValidateAssetAsync(assetId);
+
+            if (result != null)
+                throw HftApiException.Create(result.Code, result.Message).AddField(result.FieldName);
+
             var asset = await _assetsService.GetAssetByIdAsync(assetId);
             return Ok(ResponseModel<Asset>.Ok(asset));
         }
