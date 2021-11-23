2ac06ac8d5bc3d673ffba6a98f8fd8a9769c108b
Dmitry Samodov <samodov.dmitry@gmail.com>
2021-11-23 15:03:21 +0300

    renamed HistoricalId -> OperationId

--- src/Lykke.HftApi.Domain/Entities/OperationsHistory/OperationHistoricRecord.cs
+++ src/Lykke.HftApi.Domain/Entities/OperationsHistory/OperationHistoricRecord.cs
@@ -4,7 +4,7 @@ namespace Lykke.HftApi.Domain.Entities.OperationsHistory
 {
     public class OperationHistoricRecord
     {
-        public string HistoricalId { set; get; }
+        public string OperationId { set; get; }
         public string AssetId { set; get; }
         public decimal TotalVolume { set; get; }
         public decimal Fee { set; get; }
