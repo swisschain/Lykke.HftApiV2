8959bd60118b26fce2e0269ac247a4a1a7a7b0c2
Dmitry Samodov <samodov.dmitry@gmail.com>
2021-12-10 16:45:58 +0300

    LM-1710: added a tool to create the default deposit whitelist items for hft wallets

---
+++ tools/WhitelistItems/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lykke.Common.Log;
+using Lykke.Logs;
+using Lykke.Logs.Loggers.LykkeConsole;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace WhitelistItems
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using ILoggerFactory loggerFactory =
+                LoggerFactory.Create(builder =>
+                    builder.AddSimpleConsole(options =>
+                    {
+                        options.IncludeScopes = true;
+                        options.SingleLine = true;
+                        options.TimestampFormat = "hh:mm:ss ";
+                    }));
+
+            ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var settings = new AppSettings();
+
+            var logFactory = LogFactory.Create().AddConsole();
+            var log = logFactory.CreateLog("WhitelistItems");
+
+            config.Bind(settings);
+
+            var mongoUrl = new MongoUrl(settings.MongoDbConnectionString);
+            ConventionRegistry.Register("Ignore extra", new ConventionPack { new IgnoreExtraElementsConvention(true) }, x => true);
+
+            var database = new MongoClient(mongoUrl).GetDatabase(mongoUrl.DatabaseName);
+
+            var collection = database.GetCollection<ApiKey>(settings.ApiKeyCollectionName);
+
+            var service = new SiriusWalletsService(settings, log);
+
+            var now = DateTime.UtcNow;
+            var allApiKeys = (await collection.FindAsync(x => x.ValidTill == null || x.ValidTill > now)).ToList();
+
+            log.Info($"Processing {allApiKeys.Count} api keys");
+
+            foreach (var apiKey in allApiKeys)
+            {
+                try
+                {
+                    await service.CreateWalletAsync(apiKey.ClientId, apiKey.WalletId);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing walletId = {apiKey.WalletId}, clientId = {apiKey.ClientId}: {ex.Message}.");
+                }
+            }
+
+            log.Info($"Finished processing!");
+        }
+    }
+}
